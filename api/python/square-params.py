9f5487c3ce41d7f8757ef46d153dfb96439c8322
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2024-09-27 15:59:48 +0000

    add python api basic ex

---
+++ api/python/square-params.py
@@ -0,0 +1,57 @@
+"""
+from Gmsh Python tutorial 1
+
+Use built-in kernel API functions have the `gmsh.model.geo' prefix.
+"""
+
+import gmsh
+import sys
+
+gmsh.initialize()
+gmsh.model.add("square")
+
+import argparse
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('-lc', help="mesh characteristic length", type=float, default=0.1)
+parser.add_argument('-Lx', help="Length along OX", type=float, default=0.25)
+parser.add_argument('-Ly', help="Length along OY", type=float, default=0.25)
+parser.add_argument('-nopopup', action='store_true')  # on/off flag
+args = parser.parse_args()
+print(args)
+
+Lx = args.Lx
+Ly = args.Ly
+lc = args.lc
+
+p1 = gmsh.model.geo.addPoint(-Lx, -Ly, 0, lc, 1)
+p2 = gmsh.model.geo.addPoint(Lx, -Ly, 0, lc, 2)
+p3 = gmsh.model.geo.addPoint(Lx, Ly, 0, lc, 3)
+p4 = gmsh.model.geo.addPoint(-Lx, Ly, 0, lc)
+
+# Lines
+gmsh.model.geo.addLine(p1, p2, 1)
+gmsh.model.geo.addLine(p3, p2, 2)
+gmsh.model.geo.addLine(p3, p4, 3)
+gmsh.model.geo.addLine(p4, p1, p4)
+
+gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
+
+gmsh.model.geo.addPlaneSurface([1], 1)
+
+# Before meshing, the CAD entities must
+# be synchronized with the Gmsh model.
+gmsh.model.geo.synchronize()
+
+gmsh.model.addPhysicalGroup(1, [1], name="Ox")
+gmsh.model.addPhysicalGroup(1, [2, 3, 4], name="others")
+gmsh.model.addPhysicalGroup(2, [1], name="Top")
+
+# generate a 2D mesh...
+gmsh.model.mesh.generate(2)
+gmsh.write("t1.msh")
+
+# To visualize the model we can run the graphical user interface with
+if not args.nopopup:
+    gmsh.fltk.run()
+
+gmsh.finalize()
--- slidev/slides.md
+++ slidev/slides.md
@@ -1019,6 +1019,53 @@ layout: two-cols
 
 # Mesh using Gmsh API
 
+```python
+import gmsh
+import sys
+
+gmsh.initialize()
+gmsh.model.add("square")
+
+import argparse
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('-lc', help="mesh characteristic length", type=float, default=0.1)
+parser.add_argument('-Lx', help="Length along OX", type=float, default=0.25)
+parser.add_argument('-Ly', help="Length along OY", type=float, default=0.25)
+parser.add_argument('-nopopup', action='store_true')  # on/off flag
+args = parser.parse_args()
+
+Lx = args.Lx
+Ly = args.Ly
+lc = args.lc
+
+p1 = gmsh.model.geo.addPoint(-Lx, -Ly, 0, lc, 1)
+p2 = gmsh.model.geo.addPoint(Lx, -Ly, 0, lc, 2)
+p3 = gmsh.model.geo.addPoint(Lx, Ly, 0, lc, 3)
+p4 = gmsh.model.geo.addPoint(-Lx, Ly, 0, lc)
+
+# Lines
+gmsh.model.geo.addLine(p1, p2, 1)
+gmsh.model.geo.addLine(p3, p2, 2)
+gmsh.model.geo.addLine(p3, p4, 3)
+gmsh.model.geo.addLine(p4, p1, p4)
+
+gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
+
+gmsh.model.geo.addPlaneSurface([1], 1)
+gmsh.model.geo.synchronize()
+
+gmsh.model.addPhysicalGroup(1, [1], name="Ox")
+gmsh.model.addPhysicalGroup(1, [2, 3, 4], name="others")
+gmsh.model.addPhysicalGroup(2, [1], name="Top")
+
+gmsh.model.mesh.generate(2)
+gmsh.write("t1.msh")
+
+if not args.nopopup:
+    gmsh.fltk.run()
+
+gmsh.finalize()
+```
 
 ---
 layout: two-cols
